4651f991efcc025694c7b5d6e20b8dce2b1e3877
Bit7eg <stonysyrop@gmail.com>
2021-05-29 14:59:28 +0700

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        private static async Task RequestAsync(string command)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp("https://localhost:80/?command=" + command);
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            using(Stream stream = response.GetResponseStream())
+            {
+                using(StreamReader reader = new StreamReader(stream))
+                {
+                    Console.Write(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
+        public static async Task Main()
+        {
+            string command = "";
+            while (command != "exit")
+            {
+                Console.Clear();
+                try
+                {
+                    await RequestAsync(command);
+                }
+                catch (WebException)
+                {
+                    Console.WriteLine("Connection failed. Press any key to try reconnect.");
+                    Console.ReadKey();
+                    continue;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
